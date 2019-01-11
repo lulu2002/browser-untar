27ca3d8019c4d20380a3e10681ffd3009d5ea8be
Pablo Karlsson <pablo.karlsson@gmail.com>
2019-01-11 23:26:19 +0100

    * Better performance.

--- src/untar-worker.js
+++ src/untar-worker.js
@@ -17,15 +17,15 @@ class UntarWorker {
     UntarWorker.postMessage({ type: "err", message: e.message });
   };
 
-  static postMessage(message, transfere){
-    self.postMessage(message, transfere);
+  static postMessage(message, transfer){
+    self.postMessage(message, transfer);
   }
 
   static untarBuffer(arrayBuffer){
     try {
       const tarFileStream = new UntarFileStream(arrayBuffer);
       while (tarFileStream.hasNext()) {
-        const file = tarFileStream.next();
+        const file = tarFileStream._readNextFile();
         UntarWorker.postMessage({ type: "e", data: file }, [file.buffer]);
       }
 
@@ -39,37 +39,35 @@ class UntarWorker {
 // Source: https://gist.github.com/pascaldekloe/62546103a1576803dade9269ccf76330
 function decodeUTF8(bytes) {
   let s = "";
+  const bytesLength = bytes.length;
+  const sC = String.fromCharCode;
+  const bS = "Incomplete byte sequence";
   let i = 0;
-  while ( i < bytes.length ) {
+  while (i < bytesLength) {
     let c = bytes[ i++ ];
     if ( c > 127 ) {
       if ( c > 191 && c < 224 ) {
-        if ( i >= bytes.length ) {
-          throw 'UTF-8 decode: incomplete 2-byte sequence';
-        }
+        if ( i >= bytesLength ) { throw bS; }
         c = (c & 31) << 6 | bytes[ i ] & 63;
       } else if ( c > 223 && c < 240 ) {
-        if ( i + 1 >= bytes.length ) {
-          throw 'UTF-8 decode: incomplete 3-byte sequence';
-        }
+        if ( i + 1 >= bytesLength) { throw bS; }
         c = (c & 15) << 12 | (bytes[ i ] & 63) << 6 | bytes[ ++i ] & 63;
       } else if ( c > 239 && c < 248 ) {
-        if ( i + 2 >= bytes.length ) {
-          throw 'UTF-8 decode: incomplete 4-byte sequence';
-        }
+        if ( i + 2 >= bytesLength ) { throw bS; }
         c = (c & 7) << 18 | (bytes[ i ] & 63) << 12 | (bytes[ ++i ] & 63) << 6 | bytes[ ++i ] & 63;
       } else {
-        throw 'UTF-8 decode: unknown multibyte start 0x' + c.toString( 16 ) + ' at index ' + (i - 1);
+        throw 'Unknown multibyte start.';
       }
       ++i;
 
-      if ( c <= 0xffff ) s += String.fromCharCode( c );
-      else if ( c <= 0x10ffff ) {
+      if ( c <= 0xffff ) {
+        s += sC( c );
+      } else if ( c <= 0x10ffff ) {
         c -= 0x10000;
-        s += String.fromCharCode( c >> 10 | 0xd800 );
-        s += String.fromCharCode( c & 0x3FF | 0xdc00 );
+        s += sC( c >> 10 | 0xd800 );
+        s += sC( c & 0x3FF | 0xdc00 );
       } else {
-        throw 'UTF-8 decode: code point 0x' + c.toString( 16 ) + ' exceeds UTF-16 reach';
+        throw 'Code point exceeds UTF-16 reach';
       }
     }
     return s;
@@ -210,7 +208,11 @@ class UntarStream {
 class UntarFileStream {
   constructor(arrayBuffer){
     this._stream = new UntarStream(arrayBuffer);
-    this._globalPaxHeader = null;
+    this._globalPaxHeader = undefined;
+  }
+
+  destroy(){
+    this._stream
   }
 
   hasNext = () => {
@@ -218,15 +220,11 @@ class UntarFileStream {
     return this._stream.position() + 4 < this._stream.size() && this._stream.peekUint32() !== 0;
   };
 
-  next = () => {
-    return this._readNextFile();
-  };
-
   _readNextFile = () => {
     const stream = this._stream;
     let file = {};
     let isHeaderFile = false;
-    let paxHeader = null;
+    let paxHeader = undefined;
 
     const headerBeginPos = stream.position();
     const dataBeginPos = headerBeginPos + 512;
@@ -258,27 +256,17 @@ class UntarFileStream {
 
     stream.position(dataBeginPos);
 
-    // Derived from https://www.mkssoftware.com/docs/man4/pax.4.asp
-    // and https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.3.0/com.ibm.zos.v2r3.bpxa500/pxarchfm.htm
     switch (file.type) {
       case "0": // Normal file is either "0" or "\0".
       case "": // In case of "\0", readString returns an empty string, that is "".
         file.buffer = stream.readBuffer(file.size);
         break;
       case "1": // Link to another file already archived
-        // TODO Should we do anything with these?
-        break;
       case "2": // Symbolic link
-        // TODO Should we do anything with these?
-        break;
       case "3": // Character special device (what does this mean??)
-        break;
       case "4": // Block special device
-        break;
       case "5": // Directory
-        break;
       case "6": // FIFO special file
-        break;
       case "7": // Reserved
         break;
       case "g": // Global PAX header
@@ -310,11 +298,11 @@ class UntarFileStream {
       file = this._readNextFile();
     }
 
-    if (this._globalPaxHeader !== null) {
+    if (this._globalPaxHeader !== undefined) {
       this._globalPaxHeader.applyHeader(file);
     }
 
-    if (paxHeader !== null) {
+    if (paxHeader !== undefined) {
       paxHeader.applyHeader(file);
     }
 
@@ -323,6 +311,6 @@ class UntarFileStream {
 }
 
 if (typeof self !== "undefined") {
-  // We're running in a worker thread
+  // Inside the worker thread.
   self.onmessage = UntarWorker.onmessage;
 }
